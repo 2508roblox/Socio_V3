2731596479f652849c4860e0285f267910a747cb
2508roblox <trangiangzxc@gmail.com>
2023-10-30 20:07:29 +0700

    fix_room_info

--- src/pages/ChatScreen.jsx
+++ src/pages/ChatScreen.jsx
@@ -155,7 +155,7 @@ const ChatScreen = () => {
 
         <div
           className={`${
-            isExpand ? "col-span-4" : "col-span-3"
+            !isExpand ? "col-span-4" : "col-span-3"
           } transition-width duration-200 ease-in-out flex flex-col justify-start h-[90vh] border-r-1 border-primary-light dark:border-primary-dark `}
         >
           <div className="top p-3 border-b-1 border-primary-light dark:border-primary-dark flex items-center justify-between">
@@ -174,7 +174,7 @@ const ChatScreen = () => {
                 <p className="text-medium text-text-gray">@username</p>
               </div>
             </div>
-            <UilFolderOpen
+            <UilFolderOpen className="cursor-pointer"
               onClick={(e) => {
                 handleExpandRoomInfo(e);
               }}
@@ -286,82 +286,86 @@ const ChatScreen = () => {
             <UilMessage></UilMessage>
           </div>
         </div>
-
-        <div className={`col-span-1   block `}>
-          <div className="flex flex-col justify-center p-6 gap-2 items-center border-b-1 border-primary-light dark:border-primary-dark mx-3 ">
-            <Image
-              isZoomed
-              className=" rounded-full  border-[2px] border-white shadow-md"
-              width={100}
-              src={avatar}
-            />
-            <div className="flex flex-col items-center justify-center">
-              <h1 className="font-semibold">Roxie Mills</h1>
-              <p className="text-medium text-text-gray">@username_mills</p>
-            </div>
-            <div className=" flex justify-center gap-3 items-center text-lg">
-              <UilPhoneAlt size="34px" />
-              <UilVideo size="34px" />
-              <UilEnvelopeShare size="34px" />
-            </div>
-          </div>
-
-          <div className="p-6 flex flex-col gap-4">
-            <h1>Photos & Multimedia</h1>
-            <div className="grid grid-cols-3 gap-3">
-              <Image
-                alt="NextUI hero Image"
-                className=" rounded-lg"
-                src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-              />
-              <Image
-                alt="NextUI hero Image"
-                className=" rounded-lg"
-                src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-              />
-              <Image
-                alt="NextUI hero Image"
-                className=" rounded-lg"
-                src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-              />
-              <Image
-                alt="NextUI hero Image"
-                className=" rounded-lg"
-                src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-              />
-              <Image
-                alt="NextUI hero Image"
-                className=" rounded-lg"
-                src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-              />
-              <Image
-                alt="NextUI hero Image"
-                className=" rounded-lg"
-                src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-              />
-            </div>
-            <p className="text-center underline text-btn-blue text-medium dark:text-btn-yellow">
-              view all
-            </p>
-          </div>
-
-          <div className="flies p-6 flex flex-col gap-3">
-            <h1>ATTACHMENTS</h1>
-            <p>Source file</p>
-            <div className="flex gap-3 items-center">
-              <div className="bg-purple-600 rounded-md px-4 py-3">ZIP</div>
-              <p>How to create project</p>
-            </div>
-            <div className="flex gap-3 items-center">
-              <div className="bg-purple-600 rounded-md px-4 py-3">ZIP</div>
-              <p>How to create project</p>
-            </div>
-            <div className="flex gap-3 items-center">
-              <div className="bg-purple-600 rounded-md px-4 py-3">ZIP</div>
-              <p>How to create project</p>
-            </div>
-          </div>
+{/* collasap */}
+      {isExpand && 
+       
+      <div className={`col-span-1   block `}>
+      <div className="flex flex-col justify-center p-6 gap-2 items-center border-b-1 border-primary-light dark:border-primary-dark mx-3 ">
+        <Image
+          isZoomed
+          className=" rounded-full  border-[2px] border-white shadow-md"
+          width={100}
+          src={avatar}
+        />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-semibold">Roxie Mills</h1>
+          <p className="text-medium text-text-gray">@username_mills</p>
         </div>
+        <div className=" flex justify-center gap-3 items-center text-lg">
+          <UilPhoneAlt size="34px" />
+          <UilVideo size="34px" />
+          <UilEnvelopeShare size="34px" />
+        </div>
+      </div>
+
+      <div className="p-6 flex flex-col gap-4">
+        <h1>Photos & Multimedia</h1>
+        <div className="grid grid-cols-3 gap-3">
+          <Image
+            alt="NextUI hero Image"
+            className=" rounded-lg"
+            src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          />
+          <Image
+            alt="NextUI hero Image"
+            className=" rounded-lg"
+            src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          />
+          <Image
+            alt="NextUI hero Image"
+            className=" rounded-lg"
+            src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          />
+          <Image
+            alt="NextUI hero Image"
+            className=" rounded-lg"
+            src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          />
+          <Image
+            alt="NextUI hero Image"
+            className=" rounded-lg"
+            src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          />
+          <Image
+            alt="NextUI hero Image"
+            className=" rounded-lg"
+            src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          />
+        </div>
+        <p className="text-center underline text-btn-blue text-medium dark:text-btn-yellow">
+          view all
+        </p>
+      </div>
+
+      <div className="flies p-6 flex flex-col gap-3">
+        <h1>ATTACHMENTS</h1>
+        <p>Source file</p>
+        <div className="flex gap-3 items-center">
+          <div className="bg-purple-600 rounded-md px-4 py-3">ZIP</div>
+          <p>How to create project</p>
+        </div>
+        <div className="flex gap-3 items-center">
+          <div className="bg-purple-600 rounded-md px-4 py-3">ZIP</div>
+          <p>How to create project</p>
+        </div>
+        <div className="flex gap-3 items-center">
+          <div className="bg-purple-600 rounded-md px-4 py-3">ZIP</div>
+          <p>How to create project</p>
+        </div>
+      </div>
+    </div>
+    
+      }
       </main>
     </div>
   );
