3b59bce146d884c9cf2d7e04e3911fcb38b30081
2508roblox <trangiangzxc@gmail.com>
2024-02-16 21:05:50 +0700

    story scroll button

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
 import avatar from "../assets/imgs/avatar.avif";
 import story from "../assets/imgs/story.jpg";
@@ -18,9 +18,10 @@ import { Avatar } from "@nextui-org/react";
 import { UilCheck } from "@iconscout/react-unicons";
 import { UilTimes } from "@iconscout/react-unicons";
 import { UilPlus } from "@iconscout/react-unicons";
+import { UilArrowCircleRight } from '@iconscout/react-unicons'
 import { Card, CardFooter, Image, Button } from "@nextui-org/react";
 import CreatePostModal from "../components/CreatePostModal";
-import ConfettiExplosion from 'react-confetti-explosion';
+import ConfettiExplosion from "react-confetti-explosion";
 //animation
 import { motion } from "framer-motion";
 import { Input } from "@nextui-org/react";
@@ -28,31 +29,91 @@ import { Link } from "react-router-dom";
 import PostCard from "../components/PostCard";
 import StoryCard from "../components/StoryCard";
 import { useDispatch, useSelector } from "react-redux";
-import { setPosts } from '../services/slices/postSlice';
+import { setPosts } from "../services/slices/postSlice";
 import { useGetAllPostsByUserIdMutation } from "../services/slices/postApiSlice";
 
 const HomeScreen = () => {
-  const [isLiked, setIsLiked] = useState(false)
-  const auth_id = useSelector((state) => state.auth.userInfo.user._id )
-    const postsData = useSelector((state) => state.post.postData?.posts ?? []  )
-console.log('ca', postsData)
+  const [isLiked, setIsLiked] = useState(false);
+  const auth_id = useSelector((state) => state.auth.userInfo.user._id);
+  const postsData = useSelector((state) => state.post.postData?.posts ?? []);
+  console.log("ca", postsData);
   const [getPosts, { isLoading: getPostsLoading, error: getPostsError }] = useGetAllPostsByUserIdMutation();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const scrollContainerRef = useRef(null);
+
 
   useEffect(() => {
     const fetchUserPosts = async () => {
-      await getPosts(auth_id).unwrap()
-      .then( (response) => {
-            dispatch(setPosts(response))
-          console.log("check dispatch: ", postsData)
-      })
-    }
-    fetchUserPosts()
-  }, [])
- 
+      await getPosts(auth_id)
+        .unwrap()
+        .then((response) => {
+          dispatch(setPosts(response));
+          console.log("check dispatch: ", postsData);
+        });
+    };
+    fetchUserPosts();
+  }, []);
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+
+    const handleMouseDown = (event) => {
+      event.preventDefault();
+      scrollContainer.style.cursor = "grabbing";
+      scrollContainer.style.userSelect = "none";
+
+      let prevClientX = event.clientX;
+
+      const handleMouseMove = (event) => {
+        const delta = event.clientX - prevClientX;
+        scrollContainer.scrollLeft -= delta;
+        prevClientX = event.clientX;
+      };
+
+      const handleMouseUp = () => {
+        scrollContainer.style.cursor = "grab";
+        scrollContainer.style.removeProperty("user-select");
+
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+      };
+
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    scrollContainer.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      scrollContainer.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
+
+  const handleScrollButtonClick = () => {
+    const scrollContainer = scrollContainerRef.current;
+    const scrollDistance = 100; // Điều chỉnh giá trị 100 để thay đổi khoảng cách khi kéo qua
+    const scrollStep = 5; // Điều chỉnh giá trị 10 để thay đổi tốc độ kéo qua
+  
+    let scrollPosition = scrollContainer.scrollLeft;
+    let scrollTarget = scrollPosition + scrollDistance;
+    let scrollDirection = scrollTarget > scrollPosition ? 1 : -1;
+  
+    const scroll = () => {
+      scrollPosition += scrollStep * scrollDirection;
+      if (
+        (scrollDirection === 1 && scrollPosition >= scrollTarget) ||
+        (scrollDirection === -1 && scrollPosition <= scrollTarget)
+      ) {
+        scrollContainer.scrollLeft = scrollPosition;
+      } else {
+        requestAnimationFrame(scroll);
+        scrollContainer.scrollLeft = scrollPosition;
+      }
+    };
+  
+    scroll();
+  };
   return (
     <div className="bg-primary-light  transition-all duration-1000 dark:bg-primary-dark w-full min-h-screen px-6 pt-3">
-
       <Header></Header>
       <main className="grid grid-cols-5 mt-3">
         <div className="flex flex-col gap-4">
@@ -99,10 +160,10 @@ console.log('ca', postsData)
             </div>
 
             <div className="p-6 mt-2">
-              <Link to={'/profile'}>
+              <Link to={"/profile"}>
                 <Button
                   onPress={(e) => {
-                    e.preventDefault()
+                    e.preventDefault();
                   }}
                   className="w-full  bg-btn-blue rounded-md font-medium  text-xl dark:text-black  text-white p-4 "
                   size="lg"
@@ -182,12 +243,27 @@ console.log('ca', postsData)
         </div>
         {/* feeds */}
         <div
-          style={{ flexFlow: "row wrap", padding: "0 2rem" }}
+          style={{ flexFlow: "row wrap", padding: "0 2rem", position: 'relative' }}
           className="scrollbar-hide  col-span-3 flex flex-col h-[90vh] overflow-y-scroll overflow-x-visible py-5"
         >
+       
+      <UilArrowCircleRight
+       style={{
+        position: 'absolute',
+        zIndex: '10',
+        right: '3rem',
+        top: '100px',
+        
+       }}
+       width="50px"
+       height="50px"
+       onClick={handleScrollButtonClick}
+       className=" bg-secondary-light text-btn-blue   dark:bg-secondary-dark dark:text-btn-yellow rounded-full " 
+      ></UilArrowCircleRight >
           {/* story */}
-          <div className=" h-[240px] mb-4   w-full px-3 flex gap-2   overflow-x-scroll  overflow-y-hidden scrollbar-hide mx-4 ">
+          <div   ref={scrollContainerRef} className=" h-[240px] mb-4   w-full px-3 flex gap-2   overflow-x-scroll  overflow-y-hidden scrollbar-hide mx-4 ">
             {/*  */}
+         
             <div className=" story-card h-[100%]  w-[140px]   flex-shrink-0  relative shadow-md rounded-2xl  flex flex-col border-[2px] border-gray-400 border-dashed items-center justify-center gap-4">
               <Button
                 className="h-full p-3  dark:bg-transparent flex flex-col w-[100%]  bg-transparent"
@@ -202,35 +278,65 @@ console.log('ca', postsData)
                   </h1>
                 </div>
               </Button>
+    
             </div>
-            <StoryCard storyImage={'https://i.pinimg.com/564x/86/b6/53/86b653f4cc32988b33c5539b98c73ce5.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/564x/7e/26/3c/7e263c6ce3a56b562b7619e9f4c022a4.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/736x/5f/b4/54/5fb45486744f8e0e3b19fbf3d812d1a3.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/736x/35/1a/53/351a53a310f10cd6cab28d751e174beb.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/564x/66/d5/60/66d560c0fc45bba26f720da922332f4a.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/564x/d4/c2/52/d4c252afb184abbdfbcd47a3fe6dc00c.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/564x/b1/4d/dc/b14ddc3fce6074c077787b839f0b5a1d.jpg'}></StoryCard>
-            <StoryCard storyImage={'https://i.pinimg.com/736x/6e/a4/23/6ea423e7748d35c004470cf1437172a8.jpg'}></StoryCard>
-
-
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/564x/86/b6/53/86b653f4cc32988b33c5539b98c73ce5.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/564x/7e/26/3c/7e263c6ce3a56b562b7619e9f4c022a4.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/736x/5f/b4/54/5fb45486744f8e0e3b19fbf3d812d1a3.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/736x/35/1a/53/351a53a310f10cd6cab28d751e174beb.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/564x/66/d5/60/66d560c0fc45bba26f720da922332f4a.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/564x/d4/c2/52/d4c252afb184abbdfbcd47a3fe6dc00c.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/564x/b1/4d/dc/b14ddc3fce6074c077787b839f0b5a1d.jpg"
+              }
+            ></StoryCard>
+            <StoryCard
+              storyImage={
+                "https://i.pinimg.com/736x/6e/a4/23/6ea423e7748d35c004470cf1437172a8.jpg"
+              }
+            ></StoryCard>
           </div>
           {/* create post */}
           <CreatePostModal></CreatePostModal>
           {/* posts */}
-          {postsData.slice().reverse().map(post => (
-  <PostCard key={post._id} post={post}></PostCard>
-))}
-                  
-           
-           
-        
+          {postsData
+            .slice()
+            .reverse()
+            .map((post) => (
+              <PostCard key={post._id} post={post}></PostCard>
+            ))}
         </div>
         {/* etc */}
         <div className="flex flex-col gap-4   overflow-y-scroll h-[90vh]  scrollbar-hide pr-4">
           {/* ads */}
           <div className="mx-3 bg-white dark:bg-secondary-dark p-6 rounded-2xl shadow-lg relative w-full flex flex-col gap-2 ">
             <Image
-              style={{ width: '100%' }}
+              style={{ width: "100%" }}
               width={600}
               className="w-full h-[214px] object-cover scroll-m-0  "
               alt="NextUI hero Image"
@@ -320,7 +426,7 @@ console.log('ca', postsData)
             </div>
             <div className="w-full">
               <Image
-                style={{ width: '100%' }}
+                style={{ width: "100%" }}
                 width={600}
                 className="w-full h-[214px] object-cover scroll-m-0  "
                 alt="NextUI hero Image"
